d8b7db6a2c09aa64b1237e9dcd80553804fdf188
Hesham Meneisi <heshammeneisi@gmail.com>
2025-10-16 13:04:28 +0700

    feat: support multiple tags
    Refactor Docker image build command to support multiple tags.

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,10 +17,17 @@ if [ -n "$INPUT_GIT_ACCESS_TOKEN" ]; then
         GIT_ACCESS_TOKEN_FLAG="${INPUT_GIT_ACCESS_TOKEN}@"
 fi
 
-echo "Building Docker image ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} from ${GITHUB_REPOSITORY} on ${INPUT_BRANCH} and using context ${INPUT_FOLDER} ; and pushing it to ${INPUT_REGISTRY} Azure Container Registry"
+echo "Building Docker image '${INPUT_REPOSITORY}${IMAGE_PART}' with tags '${INPUT_TAG}' from '${GITHUB_REPOSITORY}' on '${INPUT_BRANCH}' and using context '${INPUT_FOLDER}' ; and pushing it to '${INPUT_REGISTRY}' Azure Container Registry"
 
 echo "Logging into azure.."
 az login --service-principal -u ${INPUT_SERVICE_PRINCIPAL} -p ${INPUT_SERVICE_PRINCIPAL_PASSWORD} --tenant ${INPUT_TENANT}
 
-echo "Sending build job to ACR.."
-az acr build -r ${INPUT_REGISTRY} ${BUILD_ARGS} -f ${INPUT_DOCKERFILE} -t ${INPUT_REPOSITORY}${IMAGE_PART}:${INPUT_TAG} https://${GIT_ACCESS_TOKEN_FLAG}github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}
+TAG_ARGS=()
+for TAG in ${INPUT_TAG}; do
+      TAG_ARGS+=("-t" "${INPUT_REPOSITORY}${IMAGE_PART}:${TAG}")
+done
+
+echo "Sending build job to ACR..."
+az acr build -r "${INPUT_REGISTRY}" ${BUILD_ARGS} -f "${INPUT_DOCKERFILE}" \
+      "${TAG_ARGS[@]}" \
+      "https://${GIT_ACCESS_TOKEN_FLAG}github.com/${GITHUB_REPOSITORY}.git#${INPUT_BRANCH}:${INPUT_FOLDER}"
